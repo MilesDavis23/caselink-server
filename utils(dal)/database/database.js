eb47904bc5d819b4e04a5daede3e9cdccbeadfc8
Miles Davis <foldes@mail.com>
2023-08-17 23:44:59 +0200

    basic dal functions for lawyer my cases intialized

---
+++ utils(dal)/database/database.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost', 
+    user: 'root',
+    password: 'petiland',
+    database: 'CaselinkDB'
+});
+
+pool.query('SELECT * FROM USERS', (error, results) => {
+    if (error) throw error;
+    console.log(results);
+});
