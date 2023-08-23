6384034cbfb2cf995cc36c3fb35bf5b9b2d2cf77
Miles Davis <foldes@mail.com>
2023-08-23 17:57:07 +0200

    minor cookie updates

--- utils(dal)/login/login.js
+++ utils(dal)/login/login.js
@@ -3,6 +3,7 @@ const pool = require('../database/database');
 function getPassword(password) {
     return new Promise((resolve, reject) => {
         const query = 'SELECT role FROM USERS WHERE password = ?';
+        /* adding issue on git */
         pool.query(query, [password], (error, results) => {
             if (error) {
                 reject(error);
