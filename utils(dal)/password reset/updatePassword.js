35d213d663426bd54385b8c40c5b1511819669b1
Miles Davis <foldes@mail.com>
2023-09-09 18:04:24 +0200

    updatePassword update

--- utils(dal)/password reset/updatePassword.js
+++ utils(dal)/password reset/updatePassword.js
@@ -1,15 +1,21 @@
 const pool = require('../database/database');
+const bcrypt = require('bcrypt');
 
 function resetPassword(email, newPassword) {
-    return new Promise((resolve, reject) => {
-        const query = 'UPDATE USERS SET password = ? WHERE email = ?';
-        pool.query(query, [newPassword, email], (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            const query = 'UPDATE USERS SET password = ? WHERE email = ?';
+            pool.query(query, [hashedPassword, email], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            })
+        } catch (error) {
+            reject(error);
+        }
     });
 }
 
