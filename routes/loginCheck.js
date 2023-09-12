e1973b35eda7b784dc497b2c564b1625c6052c35
Miles Davis <foldes@mail.com>
2023-09-12 18:10:52 +0200

    JWT implemented

--- routes/loginCheck.js
+++ routes/loginCheck.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { encrypt } = require('../utils(dal)/encryption/encryption');
 const { getUserByEmail } = require('../utils(dal)/password reset/userByEmail');
+const { generateToken } = require('../utils(dal)/token generator/token');
 const bcrypt = require('bcrypt');
 
 
@@ -17,10 +18,7 @@ router.post('/', async (req, res) => {
 
         if (!password) {
             return res.status(200).json({ message: 'Email is valid.'})
-        }
-        console.log(validUser)
-        console.log(validUser.password)
-        console.log(password)
+        } /* This is used to get the password input ready */
 
         const match = await bcrypt.compare(password, validUser.password)
         console.log(match)
@@ -28,9 +26,14 @@ router.post('/', async (req, res) => {
             return res.status(401).json({ success: false, message: 'Invalid password.'})
         }
 
-        const encryptedUserRole = encrypt(password, validUser.role);
-        res.cookie('encryptedData', encryptedUserRole, { httpOnly: true});
-        res.cookie('publicData', 'Some Public Data');
+
+        const userData = {
+            userId:  validUser.user_id,
+            role: validUser.role
+        };
+        const token = generateToken(userData);
+        /* const encryptedUserRole = encrypt(password, validUser.role); */
+        res.cookie('authToken', token, { httpOnly: true});
         /* res.cookie('data', data, { httpOnly: false }); */
         res.status(200).json({ success: true, message: "Login successful!", role: validUser.role });
 
--- utils(dal)/cases/my-cases.js
+++ utils(dal)/cases/my-cases.js
@@ -29,7 +29,7 @@ function getMyCasesByLawyerId(lawyerId) {
 
 module.exports = {
     getMyCasesByLawyerId
-}
+};
 
 /*
 getBookmarkedCasesByLawyer(lawyerId)
