97a608b1dd32923c52969d660df810ab314d5d2a
Miles Davis <foldes@mail.com>
2023-09-17 22:18:06 +0200

    handling offers implemented

--- utils(dal)/offer management/getOffer.js
+++ utils(dal)/offer management/getOffer.js
@@ -2,6 +2,7 @@ const pool = require('../database/database');
 
 const getOffer = (userId) => {
     return new Promise((resolve, reject) => {
+        /* here , maybe selet the offerID too.  */
         const query = `SELECT o.offerDetails, o.offerPrice FROM Offers AS o JOIN CASES AS c ON o.caseID = c.case_id WHERE c.user_id = ?`;
         pool.query(query, [userId], (error, results) => {
             if (error) {
--- utils(dal)/offer management/updateOffer.js
+++ utils(dal)/offer management/updateOffer.js
@@ -1,1 +1,35 @@
 const pool = require('../database/database');
+
+const updateOfferStatus = async (userId, status) => {
+    return new Promise((resolve, reject) => {
+        /* thid query basically search for the offer and changes basedon User-Clinet userID, add the offer ID too. */
+        const query = `
+        UPDATE Offers o
+        JOIN CASES c ON o.caseID = c.case_id
+        SET o.offerStatus = ?
+        WHERE c.user_id = ?;
+        `;
+        pool.query(query, [status, userId], (error, results) => {
+            if (error){
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    })
+}
+
+module.exports = {
+    updateOfferStatus
+}
+
+
+
+
+
+/*
+const updateOfferStatus = async (offerId, status) => {
+    const query = `UPDATE LawyerMyCases SET status = ? WHERE offerID = ?`;
+    return await database.execute(query, [status, offerId]);
+};
+*/
